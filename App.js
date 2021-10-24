4e4ee4398b56c4ace91bc2a10598b844490f0303
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-10-24 17:39:11 -0300

    Putting buttons on the screen still without any style

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Button from './src/components/Button';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <View style={styles.buttons}>
+        <Button label='AC' />
+        <Button label='/' />
+        <Button label='7' />
+        <Button label='8' />
+        <Button label='9' />
+        <Button label='*' />
+        <Button label='4' />
+        <Button label='5' />
+        <Button label='6' />
+        <Button label='-' />
+        <Button label='1' />
+        <Button label='2' />
+        <Button label='3' />
+        <Button label='+' />
+        <Button label='0' />
+        <Button label='.' />
+        <Button label='=' />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
 });
