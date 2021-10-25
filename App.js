c5703314a202b645d7d89b3a05d4b19c78e05b4b
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-10-25 12:13:56 -0300

    Adding display component

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 export default function App() {
+  const [ displayValue, setDisplayValue ] = useState('0');
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
+      <Display value={displayValue} />
       <View style={styles.buttons}>
         <Button label='AC' />
         <Button label='/' />
